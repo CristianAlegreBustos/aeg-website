dfba780d5a87aa8235c8d78b7e525f504f71a13f
Nahuel Alegre <nahuelalegre@MacBook-Pro-de-Nahuel.local>
2026-03-17 15:03:31 -0300

    feat: fix css styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alegre.engineering',
   output: 'server',
   adapter: cloudflare(),
   vite: {
