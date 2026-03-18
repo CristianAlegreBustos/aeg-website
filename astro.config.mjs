ba93b30013e8a5586be9426a0a046f8575df651b
Nahuel Alegre <nahuelalegre@MacBook-Pro-de-Nahuel.local>
2026-03-18 09:27:35 -0300

    seo implamentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alegre.engineering',
   output: 'server',
   adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
